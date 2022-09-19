05c0bf983c4177ec88c7a18876f565c2e29a8c82
Amanda McMullin <amanda.mcmullin@gmail.com>
2022-09-19 11:25:44 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,20 @@
+# import another_module
+# print(another_module.another_variable)
+
+# from turtle import Turtle, Screen
+# # create new instance of class from library
+# timmy = Turtle()
+# print(timmy)
+# # call methods associated with the object
+# timmy.shape("turtle")
+# timmy.color("RosyBrown2")
+# timmy.forward(100)
+#
+# my_screen = Screen()
+# # tap into attributes
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
+
+
+import prettytable
+
